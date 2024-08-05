e2621978fa297707091721eff890e88fe753e4f9
MillBung <qlsl2672@gmail.com>
2024-08-06 01:50:01 +0900

    Typescript Form 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [username, setUsername] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget : {value},
+    } = event;
+    setUsername(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    console.log(`Hello ${username}!`);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={username} type={"text"} placeholder={"username"} onChange={onChange}/>
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
