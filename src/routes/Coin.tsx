b79ace9b78040af46d54dab6879fd48f52d9f7f3
qls0439 <qls0439@gmtc.kr>
2024-10-25 14:11:35 +0900

    10/25 코인 API fetch 작업

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,66 @@
-import {useParams} from "react-router-dom";
+import {useEffect, useState } from "react";
+import {useLocation, useParams} from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
 
 interface RouteParams {
     coinId: string;
 }
 
+interface RouteState {
+    name: string;
+
+}
 function Coin(){
-    const { coinId} = useParams<RouteParams>();
-    console.log(coinId)
-    return <h1>Coin : {coinId}</h1>
+    const [loading, setLoading] = useState(true);
+    const {coinId} = useParams<RouteParams>();
+    const {state} = useLocation<RouteState>();
+    const [info, setInfo] = useState();
+    const [price, setPrice] = useState()
+    useEffect(() => {
+        (async () => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+            ).json();
+            console.log(infoData);
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+            ).json();
+            console.log(priceData);
+            setInfo(infoData);
+            setPrice(priceData);
+        })();
+    }, []);
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading..."}</Title>
+            </Header>
+            {loading ? (<Loader>Loading....</Loader>) : <span>hello</span>}
+        </Container>
+    );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,3 +1,4 @@
+import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
 
@@ -21,10 +22,11 @@ const Coin = styled.li`
   color: ${props => props.theme.bgColor};
   margin-bottom: 10px;
   border-radius: 15px;
-  a{
-    padding: 20px;
-    transition: color 0.2s ease-in;
-    display: block;
+  a {
+      display: flex;
+      align-items: center;
+      padding: 20px;
+      transition: color 0.2s ease-in;
   }
   &:hover{
     a{
@@ -36,6 +38,17 @@ const Coin = styled.li`
 const Title = styled.h1`
   font-size: 48px;
   color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -50,17 +63,40 @@ interface CoinInterface {
 
 function Coins(){
     const [coins, setCoins] = useState<CoinInterface[]>([]);
-    return <Container>
-        <Header>
-            <Title>코인</Title>
-        </Header>
-        <CoinsList>
-            {coins.map(coins => <Coin key={coins.id}>
-                <Link to={`/${coins.id}`}>{coins.name} &rarr;</Link>
-            </Coin>)}
-
-        </CoinsList>
-    </Container>
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+    }, []);
+    return (
+        <Container>
+            <Header>
+                <Title>코인</Title>
+            </Header>
+            {loading ? (
+                <Loader>Loading....</Loader>                                                                                                                                                      
+            ) : (
+                <CoinsList>
+                    {coins.map(coins => <Coin key={coins.id}>
+                        <Link to={{
+                            pathname: `/${coins.id}`,
+                            state: {
+                                name: coins.name
+                            }
+                        }}>
+                            <Img src={`https://cryptoicon-api.pages.dev/api/icon/${coins.symbol.toLowerCase()}`}/>
+                            {coins.name} &rarr;
+                        </Link>
+                    </Coin>)}
+                </CoinsList>
+            )
+            }
+        </Container>
+    );
 }
 
 export default Coins;
