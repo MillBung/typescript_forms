a4c6500c671403f78c14076ebe071b9d4e59d97f
qls0439 <qls0439@gmtc.kr>
2024-11-14 18:03:23 +0900

    11/114 코인 API fetch 작업 2

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -219,7 +219,7 @@ function Coin(){
                             <Price></Price>
                         </Route>
                         <Route path={`/:coinId/chart`}>
-                            <Chart></Chart>
+                            <Chart coinId={coinId}></Chart>
                         </Route>
                     </Switch>
                 </>
